41a7bb1d27f9d684a45a8b43ea8f1d87fd7a15d1
Mauro Porras <mauroporrasc@gmail.com>
2020-01-09 10:30:49 -0500

    wip

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use Parcel to bundle this sandbox, you can find more info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const domElement = document.getElementById("app");
+
+const scene = new ZeaEngine.Scene();
+scene.setupGrid(5.0, 50);
+
+const renderer = new ZeaEngine.GLRenderer(domElement);
+renderer.setScene(scene);
+renderer
+  .getViewport()
+  .getCamera()
+  .setPositionAndTarget(
+    new ZeaEngine.Vec3(2, 2, 1.7),
+    new ZeaEngine.Vec3(0, 0, 0.4)
+  );
+renderer.resumeDrawing();
+
+document.addEventListener("keypress", event => {
+  if (event.key == "V" && event.shiftKey) {
+    const vrvp = renderer.getVRViewport();
+    if (vrvp) vrvp.togglePresenting();
+  }
+});
