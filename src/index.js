85d4b578db965f228bb053723085ce817d1b8709
Mauro Porras <mauroporrasc@gmail.com>
2020-01-09 10:37:22 -0500

    wip

--- src/index.js
+++ src/index.js
@@ -2,22 +2,22 @@ import "./styles.css";
 
 const domElement = document.getElementById("app");
 
-const scene = new ZeaEngine.Scene();
+const scene = new window.ZeaEngine.Scene();
 scene.setupGrid(5.0, 50);
 
-const renderer = new ZeaEngine.GLRenderer(domElement);
+const renderer = new window.ZeaEngine.GLRenderer(domElement);
 renderer.setScene(scene);
 renderer
   .getViewport()
   .getCamera()
   .setPositionAndTarget(
-    new ZeaEngine.Vec3(2, 2, 1.7),
-    new ZeaEngine.Vec3(0, 0, 0.4)
+    new window.ZeaEngine.Vec3(2, 2, 1.7),
+    new window.ZeaEngine.Vec3(0, 0, 0.4)
   );
 renderer.resumeDrawing();
 
 document.addEventListener("keypress", event => {
-  if (event.key == "V" && event.shiftKey) {
+  if (event.key === "V" && event.shiftKey) {
     const vrvp = renderer.getVRViewport();
     if (vrvp) vrvp.togglePresenting();
   }
